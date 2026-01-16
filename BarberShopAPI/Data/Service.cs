1adb8b453880d75db98066a67035e60f89628fc2
thodkarik <thod.karik@gmail.com>
2026-01-16 22:51:54 +0200

    feat: add service entity

---
+++ BarberShopAPI/Data/Service.cs
@@ -0,0 +1,9 @@
+﻿namespace BarberShopAPI.Data
+{
+    public class Service : BaseEntity
+    {
+        public string Name { get; set; } = null!;
+        public int DurationMinutes { get; set; }
+        public decimal Price { get; set; }
+    }
+}
