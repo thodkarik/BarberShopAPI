f4665c01c47d9204b60bec538d05877f6e1bced6
thodkarik <thod.karik@gmail.com>
2026-01-16 22:46:57 +0200

    feat: add base entity, user, barber and customer models

---
+++ BarberShopAPI/Data/Barber.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberShopAPI.Data
+{
+    public class Barber : BaseEntity
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+
+        public int UserId { get; set; }
+        public User User { get; set; } = null!;
+    }
+}
---
+++ BarberShopAPI/Data/User.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberShopAPI.Data
+{
+    public class User : BaseEntity
+    {
+        public string Username { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public UserRole UserRole { get; set; }
+    }
+}
