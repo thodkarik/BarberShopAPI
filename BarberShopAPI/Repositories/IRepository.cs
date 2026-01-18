90ce80823bd923be7f309a9a29d72d864892709d
thodkarik <thod.karik@gmail.com>
2026-01-18 20:06:23 +0200

    feat: add generic repository

---
+++ BarberShopAPI/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace BarberShopAPI.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null);
+        Task<T?> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        Task SaveChangesAsync();
+    }
+}
---
+++ BarberShopAPI/Repositories/Repository.cs
@@ -0,0 +1,33 @@
+﻿using BarberShopAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace BarberShopAPI.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<List<T>> GetAllAsync(Expression<Func<T, bool>>? filter = null)
+        {
+            IQueryable<T> query = _dbSet;
+            if (filter != null) query = query.Where(filter);
+            return await query.ToListAsync();
+        }
+
+        public async Task<T?> GetByIdAsync(int id) => await _dbSet.FindAsync(id);
+
+        public async Task AddAsync(T entity) => await _dbSet.AddAsync(entity);
+
+        public void Update(T entity) => _dbSet.Update(entity);
+
+        public Task SaveChangesAsync() => _context.SaveChangesAsync();
+    }
+}
