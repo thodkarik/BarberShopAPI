fe27be8459747a47e5e220b9a0b5c4fa15d0213b
thodkarik <thod.karik@gmail.com>
2026-01-19 22:06:23 +0200

    feat: add appointments controller

---
+++ BarberShopAPI/Controllers/AppointmentsController.cs
@@ -0,0 +1,32 @@
+﻿using BarberShopAPI.DTO;
+using BarberShopAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BarberShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppointmentsController : ControllerBase
+    {
+        private readonly IAppointmentService _appointmentService;
+
+        public AppointmentsController(IAppointmentService appointmentService)
+        {
+            _appointmentService = appointmentService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CreateAppointmentDTO request)
+        {
+            var created = await _appointmentService.CreateAsync(request);
+            return CreatedAtAction(nameof(GetById), new { id = created.Id }, created);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return Ok();
+        }
+    }
+}
+
