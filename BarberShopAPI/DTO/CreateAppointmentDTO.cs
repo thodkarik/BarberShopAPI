9a6ea121d62d308037171e764d40ee943a7514cf
thodkarik <thod.karik@gmail.com>
2026-01-19 20:15:04 +0200

    feat: add create appointment dto

---
+++ BarberShopAPI/DTO/CreateAppointmentDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace BarberShopAPI.DTO
+{
+    public class CreateAppointmentDTO
+    {
+        public int CustomerId { get; set; }
+        public int BarberId { get; set; }
+        public int ServiceId { get; set; }
+        public DateTime AppointmentDate { get; set; }
+    }
+}
