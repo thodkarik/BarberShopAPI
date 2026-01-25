b239885225f17ca8b4b1029549bd16ce057bbae4
thodkarik <thod.karik@gmail.com>
2026-01-25 10:16:55 +0200

    feat: add user login dto

---
+++ BarberShopAPI/DTO/UserLoginDTO.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BarberShopAPI.DTO
+{
+    public class UserLoginDTO
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2,
+        ErrorMessage = "Username must be between 2 and 30 characters.")]
+        public string Username { get; set; } = null!;
+
+        [Required]
+        [RegularExpression(
+            @"(?=.*?[A-Z])(?=.*?[a-z])(?=.*?\d)(?=.*?\W)^.{8,}$",
+            ErrorMessage = "Password must contain at least one uppercase, one lowercase, one digit, and one special character")]
+        public string Password { get; set; } = null!;
+
+        public bool KeepLoggedIn { get; set; }
+    }
+}
