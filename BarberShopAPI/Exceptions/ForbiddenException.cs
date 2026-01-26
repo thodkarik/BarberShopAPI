96b94a07cd84b56393c343b8e8a8851531db3f24
thodkarik <thod.karik@gmail.com>
2026-01-26 21:46:27 +0200

    feat: add forbidden exception

---
+++ BarberShopAPI/Exceptions/ForbiddenException.cs
@@ -0,0 +1,12 @@
+﻿namespace BarberShopAPI.Exceptions
+{
+    public class ForbiddenException : BaseException
+    {
+        private static readonly string DEFAULT_CODE = "FORBIDDEN";
+
+        public ForbiddenException(string code, string message)
+            : base(code + DEFAULT_CODE, message)
+        {
+        }
+    }
+}
