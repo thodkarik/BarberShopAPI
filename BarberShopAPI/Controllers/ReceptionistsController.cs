8d8c18d0b322b29473589a76e8fa99c80af9bc5b
thodkarik <thod.karik@gmail.com>
2026-01-30 21:44:12 +0200

    feat: add admin receptionist directory endpoint

---
+++ BarberShopAPI/Controllers/ReceptionistsController.cs
@@ -0,0 +1,39 @@
+﻿using BarberShopAPI.Core.Enums;
+using BarberShopAPI.Data;
+using BarberShopAPI.DTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BarberShopAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Authorize(Roles = "Admin")]
+    public class ReceptionistsController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public ReceptionistsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<ReceptionistListItemDTO>>> GetAll()
+        {
+            var list = await _context.Users
+                .AsNoTracking()
+                .Where(u => !u.IsDeleted && u.UserRole == UserRole.Receptionist)
+                .OrderBy(u => u.LastName).ThenBy(u => u.FirstName)
+                .Select(u => new ReceptionistListItemDTO
+                {
+                    Id = u.Id,
+                    FullName = u.FirstName + " " + u.LastName
+                })
+                .ToListAsync();
+
+            return Ok(list);
+        }
+    }
+}
---
+++ BarberShopAPI/DTO/ReceptionistListItemDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace BarberShopAPI.DTO
+{
+    public class ReceptionistListItemDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; } = null!;
+    }
+}
